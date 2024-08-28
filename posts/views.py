b7775f7b1d10ece32e916613312cc49985553ac2
Anathole <anathole.karinganire@amalitech.com>
2024-08-28 18:51:51 +0200

    first commit

---
+++ posts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpRequest, JsonResponse
+
+def index(request: HttpRequest):
+    response  = {"message": "Hello, world!"}
+    return JsonResponse(data = response)
+
+# Create your views here.
